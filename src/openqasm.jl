751b7d4dab1a633625cc9324cc4e754a1df11d7e
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-11-30 20:24:14 +0100

    Refactor OpenQASM grammar rules

--- src/openqasm.jl
+++ src/openqasm.jl
@@ -12,34 +12,58 @@ rules = Dict(
         isnothing(matched) && return 0
         length(matched.match)
     end,
-    :real => Parser.scan(Base.Fix1(occursin, r"^([0-9]+\.[0-9]*|[0-9]*\.[0-9]+)([eE][-+]?[0-9]+)?")),
+    :real => Parser.scan() do input
+        capture = match(r"^([-][0-9]+\.[0-9]*|[0-9]*\.[0-9]+)([eE][-+]?[0-9]+)?", input)
+        isnothing(capture) ? 0 : length(capture.match)
+    end,
     :nninteger => Parser.some(Parser.satisfy(isdigit)),
     :ws => Parser.some(Parser.satisfy(isspace)),
-    :unaryop => Parser.first(le"sin", le"cos", le"tan", le"exp", le"ln", le"sqrt"),
-    # grammar rules
-    :mainprogram => Parser.seq(:header, :program),
+    # header
     :header => Parser.seq(
         le"OPENQASM",
         :ws,
         :version => Parser.seq(Parser.satisfy(isdigit), le".", Parser.satisfy(isdigit)),
         :eol,
     ),
-    :program => Parser.seq(Parser.many(:program), :statement),
-    :statement => Parser.first(
-        :decl,
-        Parser.seq(:gatedecl, opt(:goplist), le"}"),
-        Parser.seq(le"opaque", :id, opt(Parser.seq(le"(", opt(:idlist), le")")), :idlist, :eol),
-        :qop,
-        Parser.seq(le"if", le"(", :id, le"==", :nninteger, le")", :qop),
-        Parser.seq(le"barrier", :anylist, :eol),
-    ),
-    :decl => Parser.seq(
-        Parser.first(le"qreg", le"creg"),
+    # register declaration
+    :regdecl => Parser.seq(
+        :regdeclkind => Parser.first(le"qreg", le"creg"),
         :ws,
         :id,
-        :decl_opt_length => opt(Parser.seq(le"[", :nninteger, le"]")),
+        :regdecloptlength => opt(Parser.seq(le"[", :nninteger, le"]")),
         :eol,
     ),
+    # math expressions
+    :expr => Parser.first(
+        # TODO :id,
+        :expr_neg => Parser.seq(le"-", :expr),
+        :expr_par => Parser.seq(le"(", :expr, le")"),
+        :expr_binaryop =>
+            Parser.seq(:expr, :ws, :binaryop => Parser.first(le"+", le"-", le"*", le"/", le"^"), :ws, :expr),
+        :expr_unaryop => Parser.seq(
+            :unaryop => Parser.first(le"sin", le"cos", le"tan", le"exp", le"ln", le"sqrt"),
+            le"(",
+            :expr,
+            le")",
+        ),
+        :real,
+        :nninteger,
+        :pi => le"pi",
+    ),
+    # register reference as argument
+    :argument => Parser.first(:id, :argumentarray => Parser.seq(:id, le"[", :nninteger, le"]")),
+    # quantum operators
+    :uop => Parser.first(
+        :uopu => Parser.seq(le"U", le"(", :exprlist, le")", :argument, :eol),
+        :uopcx => Parser.seq(le"CX", :ws, :argument, le",", :argument, :eol),
+        :uopcustom => Parser.seq(:id, opt(Parser.seq(le"(", opt(:exprlist), le")")), :anylist, :eol),
+    ),
+    :qop => Parser.first(
+        :uop,
+        :qopmeasure => Parser.seq(le"measure", :argument, le"->", :argument, :eol),
+        :qopreset => Parser.seq(le"reset", :argument, :eol),
+    ),
+    # gate declaration
     :gatedecl => Parser.seq(le"gate", :id, opt(Parser.seq(le"(", opt(:idlist), le")")), :idlist, :eol),
     :goplist => Parser.first(
         :uop,
@@ -47,16 +71,7 @@ rules = Dict(
         Parser.seq(:goplist, :uop),
         Parser.seq(:goplist, le"barrier", :idlist, :eol),
     ),
-    :qop => Parser.first(
-        :uop,
-        Parser.seq(le"measure", :argument, le"->", :argument, :eol),
-        Parser.seq(le"reset", :argument, :eol),
-    ),
-    :uop => Parser.first(
-        Parser.seq(le"U", le"(", :explist, le")", :argument, :eol),
-        Parser.seq(le"CX", :argument, le",", :argument, :eol),
-        Parser.seq(:id, opt(Parser.seq(le"(", opt(:explist), le")")), :anylist, :eol),
-    ),
+    # list
     :anylist => Parser.first(:idlist, :mixedlist),
     :idlist => Parser.first(:id, Parser.seq(:idlist, le",", :id)),
     :mixedlist => Parser.first(
@@ -64,27 +79,21 @@ rules = Dict(
         Parser.seq(:mixedlist, le",", :id, opt(Parser.seq(le"[", :nninteger, le"]"))),
         Parser.seq(:idlist, le",", :id, le"[", :nninteger, le"]"),
     ),
-    :argument => Parser.first(:id, Parser.seq(:id, le"[", :nninteger, le"]")),
-    :explist => Parser.first(:exp, Parser.seq(:explist, le",", :exp)),
-    :exp => Parser.first(
-        :real,
-        :nninteger,
-        :pi => le"pi",
-        :id,
-        Parser.seq(:exp, Parser.first(le"+", le"-", le"*", le"/", le"^"), :exp),
-        Parser.seq(le"-", :exp),
-        Parser.seq(le"(", :exp, le")"),
-        Parser.seq(:unaryop, le"(", :exp, le")"),
+    :exprlist => Parser.first(:expr, Parser.seq(:exprlist, le",", :expr)),
+    # program
+    :statement => Parser.first(
+        :regdecl,
+        Parser.seq(:gatedecl, opt(:goplist), le"}"),
+        # TODO Parser.seq(le"opaque", :id, opt(Parser.seq(le"(", opt(:idlist), le")")), :idlist, :eol),
+        :qop,
+        # TODO Parser.seq(le"if", le"(", :id, le"==", :nninteger, le")", :qop),
+        # TODO Parser.seq(le"barrier", :anylist, :eol),
     ),
+    :program => Parser.some(:statement),
+    :main => Parser.seq(:header, :program),
 )
 
-grammar = Parser.make_grammar([:mainprogram], Parser.flatten(rules, Char))
-
-struct RegisterDeclaration
-    name::String
-    length::Int
-    isquantum::Bool
-end
+grammar = Parser.make_grammar([:main], Parser.flatten(rules, Char))
 
 folds = Dict(
     # terminal productions
